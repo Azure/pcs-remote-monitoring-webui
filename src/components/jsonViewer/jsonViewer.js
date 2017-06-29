eead68d0c5998d70eec9a6943fe7e48eaf1b5962
Zhang Hua <zhang-hua@users.noreply.github.com>
2017-06-29 17:01:02 +0800

    Add JsonViewer widget (#11)

---
+++ src/components/jsonViewer/jsonViewer.js
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+import React from 'react';
+import EventTopic, { Topics } from "../../common/eventtopic";
+
+import './jsonViewer.css';
+
+class JsonViewer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {}
+        };
+        this.subscriptions = [];
+    }
+
+    componentDidMount() {
+        this.subscriptions.push(EventTopic.subscribe(Topics.system.device.twin.opened, (topic, data, publisher) => {
+            this.setState({data: data});
+        }));
+    }
+
+    componentWilUnmount() {
+        EventTopic.unsubscribe(this.subscriptions);
+    }
+
+    onCopy = (e) => {
+        window.getSelection().removeAllRanges();
+        const range = document.createRange();
+        this.refs.content.focus();
+        range.selectNode(this.refs.content);
+        window.getSelection().addRange(range);
+        document.execCommand('copy');
+    }
+
+    render() {
+        return (
+            <div className="jsonViewer-tile">
+                <pre ref="content" className="jsonViewer-content">{JSON.stringify(this.state.data, null, '  ')}</pre>
+                <div>
+                    <button className="btn btn-default jsonViewer-button"  onClick={this.onCopy}>Copy</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default JsonViewer;
