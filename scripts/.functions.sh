4dbd03f59708027c98a931d620b7be0c3dbbae8f
Parvez <parvezp@microsoft.com>
2017-07-10 13:27:32 -0700

    Adding docker scripts for creating image (#43)
    * Adding docker scripts for creating image
    
    * Fixed missing docker image value
    
    * Addressing comments

---
+++ scripts/.functions.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+COL_NO="\033[0m" # no color
+COL_ERR="\033[1;31m" # light red
+COL_H1="\033[1;33m" # yellow
+COL_H2="\033[1;36m" # light cyan
+
+header() {
+    echo -e "${COL_H1}\n### $1 ${COL_NO}"
+}
+
+error() {
+    echo -e "${COL_ERR}$1 ${COL_NO}"
+}
+
+check_dependency_docker() {
+    set +e
+    TEST=$(which docker)
+    if [[ -z "$TEST" ]]; then
+        echo "ERROR: 'docker' command not found."
+        echo "Install Docker and make sure the 'docker' command is in the PATH."
+        echo "Docker installation: https://www.docker.com/community-edition#/download"
+        exit 1
+    fi
+    set -e
+}
---
+++ scripts/docker/build.cmd
@@ -0,0 +1,54 @@
+@ECHO off
+setlocal
+
+:: Note: use lowercase names for the Docker images
+SET DOCKER_IMAGE="azureiotpcs/remote-monitoring-webui:0.1-SNAPSHOT"
+
+:: Debug|Release
+SET CONFIGURATION=Release
+
+:: strlen("\scripts\docker\") => 16
+SET APP_HOME=%~dp0
+SET APP_HOME=%APP_HOME:~0,-16%
+cd %APP_HOME%
+
+:: Check dependencies
+docker version > NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 GOTO MISSING_DOCKER
+
+:: Restore packages and build the application
+call npm install
+IF %ERRORLEVEL% NEQ 0 GOTO FAIL
+call npm run build
+IF %ERRORLEVEL% NEQ 0 GOTO FAIL
+
+:: Build the container image
+rmdir /s /q out\docker
+
+mkdir out/docker/build
+
+xcopy /s build\*       out\docker\build\
+
+copy scripts\docker\.dockerignore               out\docker\
+copy scripts\docker\Dockerfile                  out\docker\
+
+cd out\docker\
+docker build --tag %DOCKER_IMAGE% --squash --compress --label "Tags=azure,iot,pcs,webui,react" .
+IF %ERRORLEVEL% NEQ 0 GOTO FAIL
+
+:: - - - - - - - - - - - - - -
+goto :END
+
+:MISSING_DOCKER
+    echo ERROR: 'docker' command not found.
+    echo Install Docker and make sure the 'docker' command is in the PATH.
+    echo Docker installation: https://www.docker.com/community-edition#/download
+    exit /B 1
+
+:FAIL
+    echo Command failed
+    endlocal
+    exit /B 1
+
+:END
+endlocal
