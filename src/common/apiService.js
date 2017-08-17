3a9fcba58610bfb0a95f9bcb25c1bb94a38528cd
Hugh Xiong <maple10001@users.noreply.github.com>
2017-08-17 09:41:03 -0700

    clean up old services endpoint (#190)
    * clean up old services endpoint
    
    * add hostname as baseURL

--- src/common/apiService.js
+++ src/common/apiService.js
@@ -3,40 +3,40 @@ import Config from './config';
 
 class ApiService {
   static getAllDevices() {
-    return Http.get(`${Config.iotHubManagerApiUrl_new}/devices`);
+    return Http.get(`${Config.iotHubManagerApiUrl}/devices`);
   }
 
   static loadTelemetryMessagesByDeviceIds(deviceIds) {
     var csvIds = deviceIds.reduce((prevStr, id) => {
       return (prevStr ? prevStr + ',' : '') + encodeURIComponent(id);
     });
-    return Http.get(`${Config.telemetryApiUrl_new}messages?devices=${csvIds}`);
+    return Http.get(`${Config.telemetryApiUrl}messages?devices=${csvIds}`);
   }
 
   static getLastTelemetryMessage(deviceId) {
     return Http.get(
-      `${Config.telemetryApiUrl_new}messages?order=desc&limit=1&devices=${encodeURIComponent(
+      `${Config.telemetryApiUrl}messages?order=desc&limit=1&devices=${encodeURIComponent(
         deviceId
       )}`
     );
   }
 
   static loadTelemetryMessages() {
-    return Http.get(`${Config.telemetryApiUrl_new}messages?order=desc`);
+    return Http.get(`${Config.telemetryApiUrl}messages?order=desc`);
   }
 
   static getTelemetryMessagesP1M() {
     return Http.get(
-      `${Config.telemetryApiUrl_new}messages?from=NOW-PT1M&to=NOW&order=desc`
+      `${Config.telemetryApiUrl}messages?from=NOW-PT1M&to=NOW&order=desc`
     );
   }
 
   static getAlarmList() {
-    return Http.get(`${Config.telemetryApiUrl_new}alarmsbyrule`);
+    return Http.get(`${Config.telemetryApiUrl}alarmsbyrule`);
   }
 
   static getRegionByDisplayName() {
-    return Http.get(`${Config.uiConfigApiUrl_new}devicegroups`);
+    return Http.get(`${Config.uiConfigApiUrl}devicegroups`);
   }
 }
 
--- src/common/config.js
+++ src/common/config.js
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 
-// TODO: clean up old services
+const baseURL = process.env.REACT_APP_BASE_URL || window.location.hostname;
+
 const Config = {
   STATUS_CODES: {
     CRITICAL: 'critical',
@@ -12,18 +13,10 @@ const Config = {
     TELEMETRY_UPDATE_INTERVAL: 2500,
     TELEMETRY_SLIDE_WINDOW: 15
   },
-  solutionApiUrl: process.env.REACT_APP_OLD_UICONFIG_WEBSERVICE_URL,
-  uiConfigApiUrl: process.env.REACT_APP_OLD_UICONFIG_WEBSERVICE_URL,
-  deviceSimulationApiUrl: process.env.REACT_APP_DEVICESIMULATION_WEBSERVICE_URL,
-  iotHubManagerApiUrl: process.env.REACT_APP_IOTHUBMANAGER_WEBSERVICE_URL,
-  telemetryApiUrl: process.env.REACT_APP_TELEMETRY_WEBSERVICE_URL,
-  deviceGroupApiUrl:
-    process.env.REACT_APP_OLD_UICONFIG_WEBSERVICE_URL +
-    'api/v1/devicegroups/list',
-  telemetryTypeApiUrl:
-    process.env.REACT_APP_OLD_UICONFIG_WEBSERVICE_URL + 'api/v1/telemetrytypes',
-  telemetryApiUrl_new: process.env.REACT_APP_TELEMETRY_WEBSERVICE_URL,
-  iotHubManagerApiUrl_new: process.env.REACT_APP_IOTHUBMANAGER_WEBSERVICE_URL,
-  uiConfigApiUrl_new: process.env.REACT_APP_UICONFIG_WEBSERVICE_URL
+  uiConfigApiUrl: `${baseURL}uiconfig/v1/`,
+  deviceSimulationApiUrl: `${baseURL}devicesimulation/v1/`,
+  iotHubManagerApiUrl: `${baseURL}iothubmanager/v1/`,
+  telemetryApiUrl: `${baseURL}devicetelemetry/v1/`
 };
+
 export default Config;
