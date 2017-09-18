9913025490be97bf13e15efb9c2b419cd7ab5d1d
Devis Lucato <dluc@users.noreply.github.com>
2017-09-18 14:55:31 -0700

    Fix git configuration (#359)
    * Fix .gitattributes syntax
    * Remove old code

--- scripts/docker/content/run.sh
+++ scripts/docker/content/run.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-# build the app - this adds 1-2 mins to the bootstrap
-# this step is required only if there are environment
-# variables to inject, which should preferrably be
-# fetched from UI config
-### echo Base Service URL: $REACT_APP_BASE_SERVICE_URL
-### npm run build
-
 # serve the app via nginx
 mkdir -p /app/logs
 nginx -g 'daemon off;' -c /app/nginx.conf
