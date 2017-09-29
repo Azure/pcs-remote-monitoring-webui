c3b2e87c12c7d21422d119ab9214ceafe7b1d5f4
Devis Lucato <devis@lucato.it>
2017-09-29 12:59:53 -0700

    Update development scripts to run the solution in localhost

--- scripts/localhost/start-dotnet.sh
+++ scripts/localhost/start-dotnet.sh
@@ -11,8 +11,17 @@ APP_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && cd .. && pwd )/"
 
 cd $APP_HOME
 
+export PCS_AUTH_REQUIRED=false
+export PCS_CORS_WHITELIST="{ 'origins': ['*'], 'methods': ['*'], 'headers': ['*'] }"
+
+# Store your development secrets in mysecrets_.sh and make sure you
+# don't check it into git history. ".gitignore" should protect from that.
+if [ -f "mysecrets_.sh" ]; then
+  . mysecrets_.sh
+fi
+
 echo "Starting PCS microservices..."
-docker-compose -f scripts/localhost/docker-compose.yml up -d --timeout 0
+docker-compose -f scripts/localhost/docker-compose.dotnet.yml up -d --timeout 0
 
 echo "Starting reverse proxy at http://127.0.0.1:9000 ..."
 nginx -c $APP_HOME/scripts/localhost/nginx.conf
