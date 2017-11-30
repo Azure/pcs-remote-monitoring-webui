d2c1c993d492b3cf642dda20cf8aae2fa1b370a7
Swetha <sangepuswetha@gmail.com>
2017-11-30 11:48:15 -0800

    Removed connected icon (#837)
    * removed connected icon
    
    * removed unused import

--- src/components/cellRenderers/connectionStatusRenderer/connectionStatusRenderer.js
+++ src/components/cellRenderers/connectionStatusRenderer/connectionStatusRenderer.js
@@ -3,7 +3,6 @@
 import React from "react";
 import lang from "../../../common/lang";
 
-import EnabledSvg from '../../../assets/icons/Enabled.svg';
 import DisabledSvg from '../../../assets/icons/Disabled.svg';
 
 import '../cellRenderer.css'
@@ -15,7 +14,7 @@ class ConnectionStatusRenderer extends React.Component {
 
     return (
       <div className={cellClasses}>
-        <img src={value ? EnabledSvg : DisabledSvg} className="pcs-renderer-icon" alt='Connection Status Icon' />
+        {value ? null :<img src={DisabledSvg} className="pcs-renderer-icon" alt='Connection Status Icon'/>}
         <div className="pcs-renderer-text">
           {value ? lang.CONNECTED : lang.OFFLINE}
         </div>
