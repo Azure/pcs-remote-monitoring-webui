671431f8da905d94995347022d2243093b0f7753
Mary Ellen Chaffin <mechaffin@gmail.com>
2018-05-07 14:52:15 -0700

    Send Enabled = true when creating a new device (#996)

--- src/services/models/iotHubManagerModels.js
+++ src/services/models/iotHubManagerModels.js
@@ -154,6 +154,7 @@ export const toNewDeviceRequestModel = ({
   return {
     Id: isGenerateId ? '' : deviceId,
     IsSimulated: isSimulated,
+    Enabled: true,
     Authentication:
       isGenerateKeys
         ? {}
