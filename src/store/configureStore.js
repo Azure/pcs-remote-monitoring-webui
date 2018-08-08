33b258faf440dbe2c932a517b183b4a88ef31da9
Stephen Pryor <stpryor@microsoft.com>
2018-08-07 17:21:49 -0700

    Enable redux devtools (#1055)

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,6 +2,7 @@
 
 import { createStore, applyMiddleware } from 'redux';
 import { createEpicMiddleware } from 'redux-observable';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootEpic from './rootEpic';
 import rootReducer from './rootReducer';
 
@@ -11,6 +12,8 @@ export default function configureStore() {
   // Initialize the redux store with middleware
   return createStore(
     rootReducer,
-    applyMiddleware(epicMiddleware)
+    composeWithDevTools(
+      applyMiddleware(epicMiddleware)
+    )
   );
 }
